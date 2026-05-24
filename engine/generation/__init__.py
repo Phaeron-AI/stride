44a71679f081a0bbe3bf7eaf2239b6284d9067c5
Aatti13 <aattreya.ks@gmail.com>
2026-05-24 21:19:19 +0530

    Added the Generation init file for making imports easier

---
+++ engine/generation/__init__.py
@@ -0,0 +1,14 @@
+from generation.code_generation_head import CodeGenerationHead, CodeGenerationOutput
+from generation.syntax_verifier import SyntaxVerifier, SyntaxVerifierOutput
+from generation.test_generator import EncoderLayer, DecoderLayer, TestGenerator, TestGeneratorOutput
+
+__all__ = [
+  "CodeGenerationHead",
+  "CodeGenerationOutput",
+  "SyntaxVerifier",
+  "SyntaxVerifierOutput",
+  "EncoderLayer",
+  "DecoderLayer",
+  "TestGenerator",
+  "TestGeneratorOutput"
+]
